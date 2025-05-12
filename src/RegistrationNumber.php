12ebfe678ed2554fb5902c60d99a53414bec0ad1
Li Zhineng <im@zhineng.li>
2025-05-12 19:01:57 +0800

    first commit

---
+++ src/RegistrationNumber.php
@@ -0,0 +1,253 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zhineng\VehicleLicenseChina;
+
+final readonly class RegistrationNumber
+{
+    /**
+     * Pairs of region name and authority codes.
+     *
+     * @var string[][]
+     */
+    private const array AUTHORITIES = [
+        '京' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        ],
+        '津' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        ],
+        '冀' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'R', 'T',
+        ],
+        '晋' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'H', 'J', 'K', 'L', 'M',
+        ],
+        '蒙' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M',
+        ],
+        '辽' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N',
+            'P',
+        ],
+        '吉' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K',
+        ],
+        '黑' => [
+            'A', 'L', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'M', 'N',
+            'P', 'R',
+        ],
+        '沪' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        ],
+        '苏' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N',
+        ],
+        '浙' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L',
+        ],
+        '皖' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N',
+            'P', 'R', 'S',
+        ],
+        '闽' => [
+            'A', 'K', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J',
+        ],
+        '赣' => [
+            'A', 'M', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L',
+        ],
+        '鲁' => [
+            'A', 'B', 'U', 'C', 'D', 'E', 'F', 'Y', 'G', 'V', 'H', 'J', 'K',
+            'L', 'M', 'N', 'P', 'Q', 'R', 'S',
+            'W', // Reserved for internal
+        ],
+        '豫' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N',
+            'P', 'Q', 'R', 'S', 'U',
+        ],
+        '鄂' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N',
+            'P', 'Q', 'R', 'S',
+        ],
+        '湘' => [
+            'A', 'S', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M',
+            'N', 'U',
+        ],
+        '粤' => [
+            'A', 'B', 'C', 'D', 'E', 'X', 'Y', 'F', 'G', 'H', 'J', 'K', 'L',
+            'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+            'Z', // Reserved for internal
+        ],
+        '桂' => [
+            'A', 'B', 'C', 'H', 'D', 'E', 'F', 'G', 'J', 'K', 'L', 'M', 'N',
+            'P', 'R',
+        ],
+        '琼' => [
+            'A', 'B', 'C', 'D', 'E', 'F',
+        ],
+        '渝' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        ],
+        '川' => [
+            'A', 'G', 'B', 'C', 'D', 'E', 'F', 'H', 'J', 'K', 'L', 'M', 'Q',
+            'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        ],
+        '贵' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J',
+        ],
+        '云' => [
+            'A', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P',
+            'Q', 'R', 'S',
+        ],
+        '藏' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G',
+        ],
+        '陕' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'V',
+        ],
+        '甘' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N',
+            'P',
+        ],
+        '青' => [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        ],
+        '宁' => [
+            'A', 'B', 'C', 'E',
+        ],
+        '新' => [
+            'A', 'B', 'C', 'E', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N',
+            'P', 'Q', 'R',
+        ],
+    ];
+
+    /**
+     * @var string[]
+     */
+    private const array CLEAN_ENERGY_FIRST_LETTERS = [
+        'D', 'A', 'B', 'C', 'E',
+        'F', 'G', 'H', 'J', 'K',
+    ];
+
+    private function __construct(
+        public string $registrationNumber
+    ) {
+        //
+    }
+
+    public static function make(string $registrationNumber): static
+    {
+        static::validateAuthority($registrationNumber);
+        static::validateSequence($registrationNumber);
+
+        return new self($registrationNumber);
+    }
+
+    private static function validateAuthority(string $registrationNumber): void
+    {
+        $region = mb_substr($registrationNumber, 0, 1);
+        $authorities = self::AUTHORITIES[$region] ?? [];
+
+        if ($authorities === []) {
+            static::notValid($registrationNumber);
+        }
+
+        $code = mb_substr($registrationNumber, 1, 1);
+
+        if ($code === 'O') {
+            return;
+        }
+
+        if (! in_array($code, $authorities)) {
+            static::notValid($registrationNumber);
+        }
+    }
+
+    private static function validateSequence(string $registrationNumber): void
+    {
+        $sequence = mb_strtoupper(mb_substr($registrationNumber, 2));
+
+        $valid = match (mb_strlen($sequence)) {
+            4, 5 => static::checkSequenceForGeneral($sequence),
+            6 => static::checkSequenceForCleanEnergy($sequence),
+            default => static::notValid($registrationNumber),
+        };
+
+        if (! $valid) {
+            static::notValid($registrationNumber);
+        }
+    }
+
+    private static function checkSequenceForGeneral(string $sequence): bool
+    {
+        $letters = 0;
+
+        for ($i = 0; $i < strlen($sequence); $i++) {
+            $codepoint = mb_ord($sequence[$i]);
+
+            if ($codepoint === false) {
+                return false;
+            }
+
+            if ($codepoint >= ord('A') && $codepoint <= ord('Z')) {
+                if ($codepoint === ord('O') || $codepoint === ord('I')) {
+                    return false;
+                }
+
+                $letters++;
+            } else if ($codepoint < ord('0') || $codepoint > ord('9')) {
+                return false;
+            }
+        }
+
+        return $letters <= 2;
+    }
+
+    private static function checkSequenceForCleanEnergy(string $sequence): bool
+    {
+        $sequence = mb_strtoupper($sequence);
+        $first = mb_substr($sequence, 0, 1);
+
+        if (! in_array($first, self::CLEAN_ENERGY_FIRST_LETTERS)) {
+            return false;
+        }
+
+        $second = mb_substr($sequence, 1, 1);
+        $second = mb_ord($second);
+
+        if ($second === false) {
+            return false;
+        }
+
+        if (($second < ord('A') || $second > ord('Z'))
+            && ($second < ord('0') || $second > ord('9'))) {
+            return false;
+        }
+
+        if ($second === ord('O') || $second === ord('I')) {
+            return false;
+        }
+
+        for ($i = 2; $i < strlen($sequence); $i++) {
+            $codepoint = mb_ord($sequence[$i]);
+
+            if ($codepoint < ord('0') || $codepoint > ord('9')) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static function notValid(string $registrationNumber): never
+    {
+        throw new RegistrationNumberException(sprintf(
+            'The registration number "%s" is invalid.', $registrationNumber
+        ));
+    }
+}
---
+++ tests/RegistrationNumberTest.php
@@ -0,0 +1,141 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zhineng\VehicleLicenseChina\Tests;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use Zhineng\VehicleLicenseChina\RegistrationNumber;
+use Zhineng\VehicleLicenseChina\RegistrationNumberException;
+
+#[CoversClass(RegistrationNumber::class)]
+final class RegistrationNumberTest extends TestCase
+{
+    #[DataProvider('provide_valid_registration_numbers')]
+    #[DataProvider('provide_valid_clean_energy_registration_numbers')]
+    public function test_valid_registration_number(string $registrationNumber): void
+    {
+        $instance = RegistrationNumber::make($registrationNumber);
+        $this->assertSame($registrationNumber, $instance->registrationNumber);
+    }
+
+    #[DataProvider('provide_invalid_registration_numbers')]
+    #[DataProvider('provide_invalid_clean_energy_registration_numbers')]
+    public function test_invalid_registration_number(string $registrationNumber): void
+    {
+        $this->expectException(RegistrationNumberException::class);
+        $this->expectExceptionMessage(sprintf('The registration number "%s" is invalid.', $registrationNumber));
+        RegistrationNumber::make($registrationNumber);
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function provide_valid_registration_numbers(): array
+    {
+        return [
+            ['粤E1234'],
+            ['粤EA123'],
+            ['粤E1A23'],
+            ['粤E12A3'],
+            ['粤E123A'],
+            ['粤EAB12'],
+            ['粤E12AB'],
+            ['粤E1AB2'],
+            ['粤E1A2B'],
+            ['粤EA12B'],
+            ['粤EA1B2'],
+            ['粤E12345'],
+            ['粤EA1234'],
+            ['粤E1A234'],
+            ['粤E12A34'],
+            ['粤E123A4'],
+            ['粤E1234A'],
+
+            // Authority code "O" (the 2nd character) is for internal
+            ['粤O12345'],
+
+            // Authority code "Z" (the 2nd character) is for internal in Guangdong province
+            ['粤Z12345'],
+
+            // Authority code "W" (the 2nd character) is for internal in Shandong province
+            ['鲁W12345'],
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function provide_valid_clean_energy_registration_numbers(): array
+    {
+        return [
+            ['粤ED12345'],
+            ['粤EDA1234'],
+
+            // Authority code "O" (the 2nd character) is for internal
+            ['粤OD12345'],
+
+            // Authority code "Z" (the 2nd character) is for internal in Guangdong province
+            ['粤ZD12345'],
+
+            // Authority code "W" (the 2nd character) is for internal in Shandong province
+            ['鲁WD12345'],
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function provide_invalid_registration_numbers(): array
+    {
+        return [
+            ['假E12345'],
+            ['粤I12345'],
+            ['粤EABC1'],
+            ['粤E123+'],
+            ['粤E1234+'],
+            ['粤EABC12'],
+
+            // Sequence could not contain "O" or "I"
+            ['粤EO123'],
+            ['粤E1O23'],
+            ['粤E12O3'],
+            ['粤E123O'],
+            ['粤EI123'],
+            ['粤E1I23'],
+            ['粤E12I3'],
+            ['粤E123I'],
+            ['粤EO1234'],
+            ['粤E1O234'],
+            ['粤E12O34'],
+            ['粤E123O4'],
+            ['粤E1234O'],
+            ['粤EI1234'],
+            ['粤E1I234'],
+            ['粤E12I34'],
+            ['粤E123I4'],
+            ['粤E1234I'],
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function provide_invalid_clean_energy_registration_numbers(): array
+    {
+        return [
+            ['粤ED1A234'],
+            ['粤ED12A34'],
+            ['粤ED123A4'],
+            ['粤ED1234A'],
+            ['粤EZ12345'],
+            ['粤ED1234+'],
+
+            // Sequence could not contain "O" or "I"
+            ['粤EDO1234'],
+            ['粤EDI1234'],
+        ];
+    }
+}
